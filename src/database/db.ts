81e62247696513c6da58ed3a5b31a3748135a833
Fran <franrocher95@gmail.com>
2024-02-26 20:00:00 +0100

    feat:creating develop/master branch and adding tables in typeorm migrations via datasources

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,10 @@
 import "reflect-metadata"
 import 'dotenv/config'
 import { DataSource } from "typeorm"
+import { Services1708971480331 } from "./migrations/1708971480331-services"
+import { Roles1708971996264 } from "./migrations/1708971996264-roles"
+import { Users1708972302419 } from "./migrations/1708972302419-users"
+import { Appointments1708973379930 } from "./migrations/1708973379930-appointments"
 
 
 
@@ -10,9 +14,9 @@ host:process.env.DB_HOST|| "localhost",
 port:Number(process.env.DB_PORT)|| 3307,
 username:process.env.DB_USER|| "root",
 password:process.env.DB_PASSWORD|| "1234",
-database:process.env.DB_DATABASE|| "typeOrmTest",
+database:process.env.DB_DATABASE|| "tattooAndPiercingShop",
 entities: [],
-migrations:[],
+migrations:[Services1708971480331,Roles1708971996264,Users1708972302419,Appointments1708973379930],
 synchronize: false,
 logging: false,
 })
---
+++ src/database/migrations/1708971480331-services.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Services1708971480331 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "services",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "services_name",
+                        type: "varchar",
+                        length: "50",
+                        isNullable: false,
+                        isUnique: true,
+
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                        length: "200",
+                        isNullable: false
+                    },
+          
+                ],
+            }),
+            true
+        );
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
